e4ed3802175711d92bd074b106c74048f4f48f27
mrgopalakrishna <46185555+mrgopalakrishna@users.noreply.github.com>
2018-12-28 17:30:16 +0530

    Update testvoot.java

--- regression_Negative_nonMandatory/src/test/java/testPack_voot/testvoot.java
+++ regression_Negative_nonMandatory/src/test/java/testPack_voot/testvoot.java
@@ -49,9 +49,11 @@ public class testvoot extends GenericFunction {
 		String plat_ios = "iOS";
 
 		
-		String web_path = "C:\\Users\\iFocus\\eclipse-workspace\\regression_Negative_nonMandatory\\Input_Files\\News_Web1_Test.xlsx";
+		//String web_path = "C:\\Users\\iFocus\\eclipse-workspace\\regression_Negative_nonMandatory\\Input_Files\\News_Web1_Test.xlsx";
 		//C:\Program Files (x86)\Jenkins\workspace\test_automation\regression_Negative_nonMandatory\Input_Files\News_Web1_Test.xlsx
 		//String web_path = "C:\\Program Files (x86)\\Jenkins\\workspace\\test_automation\\regression_Negative_nonMandatory\\Input_Files\\News_Web1_Test.xlsx";
+		
+		String web_path ="D:\\jenkins\\News_Web1_Test.xlsx";
 		String plat_web = "Web";
 
 		String pwa_path = "C:\\Users\\iFocus\\eclipse-workspace\\regression_Negative_nonMandatory\\Input_Files\\PWA_Negative.xlsx";
